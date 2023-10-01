cfc770b87216de063a711d4fae560430e277bca0
R-Perea <richard.perea.a@gmail.com>
2023-09-30 23:28:41 -0300

    arreglado el inicio mas no los botones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { environment  } from '../environments/environment';
-import { initializeApp } from 'firebase/app';
-import { LoginComponent } from './home/login/login.component'; 
-import { RouterModule, Routes } from '@angular/router';
+import { environment } from '../environments/environment';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, 
-  { path: 'login.html', component: LoginComponent },
-
-];
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { LoginComponent } from './home/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    
+    AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor() {
-    initializeApp(environment.firebaseConfig);
-  }
-}
-
-
+export class AppModule { }
